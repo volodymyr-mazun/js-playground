145298be91d7cdf775abb33988e14d90bd4ae06b
Mazun Volodymyr <mebelmv1990@gmail.com>
2026-05-16 12:27:38 +0300

    task 15

---
+++ js/module-1.js
@@ -0,0 +1,26 @@
+//TODO =============== TASK-1 ===============
+// const firstName = "Shevchenko";
+// const lastName = "Taras";
+// const fullName = `${firstName} ${lastName}`;
+
+//TODO =============== TASK-2 ===============
+// const a = 111;
+// const b = 222;
+// const sum = a + b;
+// const difference = b - caches;
+// const product = a * b;
+// const quotient = b / a;
+
+//TODO =============== TASK-3 ===============
+// const radius = 5;
+// const pi = 3.14;
+// const area = pi * radius ** 2;
+
+//TODO =============== TASK-4 ===============
+// const someString = "I like the profession of developer";
+// console.log(someString.length);
+
+//TODO =============== TASK-5 ===============
+// const someString = "I like the profession of developer";
+// const firstLetter = someString[0];
+// const lastLetter = someString[someString.length - 1];
